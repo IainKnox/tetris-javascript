2fbe455ff25074da8573564cc62631343672e271
Iain Knox <iain.knox1@btinternet.com>
2021-10-28 09:57:07 +0000

    create arrays with the various tetromino shapes

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
-document.addEventListener('DOMContentLoaded', function(){
-    console.log('All Connected!');
+document.addEventListener('DOMContentLoaded', function () {
+    const grid = document.getElementsByClassName('grid');
+    let blox = Array.from(document.querySelectorAll('.grid div'));
+    const width = 10;
 
-})
+    
+    /**
+     * Create the arrays for each of the 5 Tetromino shapes
+     * represented by l, z, t, b and i
+     */
+    const lTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [width * 2, width * 2 + 1, width + 1, 1],
+        [0, width, width + 1, width + 2]
+    ];
+
+    const zTetromino = [
+        [width * 2, width * 2 + 1, width + 1, width + 2],
+        [0, width, width + 1, width * 2 + 1],
+        [2, 1, width + 1, width],
+        [width * 2 + 2, width + 2, width + 1, 1]
+    ];
+
+    const tTetromino = [
+        [1, width, width + 1, width + 2],
+        [width + 2, 1, width + 1, width * 2 + 1],
+        [width * 2 + 1, width, width + 1, width + 2],
+        [width, 1, width + 1, width * 2 + 1]
+    ];
+
+    const bTetromino = [
+        [0, 1, width, width + 1],
+        [1, 2, width + 1, width + 2],
+        [width + 1, width + 2, width * 2 + 1, width * 2 + 2],
+        [width, width + 1, width * 2, width * 2 + 1]
+    ];
+
+    const iTetromino = [
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [2, width + 2, width * 2 + 2, width * 3 + 2],
+        [width * 2, width * 2 + 1, width * 2 + 2, width * 2 + 3]
+    ];
+
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, bTetromino, iTetromino];
+    console.log(theTetrominoes[0][3]);
+
+
+
+
+});
