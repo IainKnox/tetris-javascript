0f768907e6dc192e14d19ec59bb8d962c7af2b11
Iain Knox <iain.knox1@btinternet.com>
2021-11-01 00:43:24 +0000

    add function to move the tetromino down when pressing the down arrow key

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,44 +5,44 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(grid);
     console.log(blox); //checking to make sure the array is correct
 
-    
+
     /**
      * Create the arrays for each of the 5 Tetromino shapes
      * represented by l, z, t, b and i
      */
     const lTetromino = [
-        [1, width+1, width*2+1, 2],
-        [width, width+1, width+2, width*2+2],
-        [width*2, width*2+1, width+1, 1],
-        [0, width, width+1, width+2]
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [width * 2, width * 2 + 1, width + 1, 1],
+        [0, width, width + 1, width + 2]
     ];
 
     const zTetromino = [
-        [width*2, width*2+1, width+1, width+2],
-        [0, width, width+1, width*2+1],
-        [2, 1, width+1, width],
-        [width*2+2, width+2, width+1, 1]
+        [width * 2, width * 2 + 1, width + 1, width + 2],
+        [0, width, width + 1, width * 2 + 1],
+        [2, 1, width + 1, width],
+        [width * 2 + 2, width + 2, width + 1, 1]
     ];
 
     const tTetromino = [
-        [1, width, width+1, width+2],
-        [width+2, 1, width+1, width*2+1],
-        [width*2+1, width, width+1, width+2],
-        [width, 1, width+1, width*2+1]
+        [1, width, width + 1, width + 2],
+        [width + 2, 1, width + 1, width * 2 + 1],
+        [width * 2 + 1, width, width + 1, width + 2],
+        [width, 1, width + 1, width * 2 + 1]
     ];
 
     const bTetromino = [
-        [0, 1, width, width+1],
-        [1, 2, width+1, width+2],
-        [width+1, width+2, width*2+1, width*2+2],
-        [width, width+1, width*2, width*2+1]
+        [0, 1, width, width + 1],
+        [1, 2, width + 1, width + 2],
+        [width + 1, width + 2, width * 2 + 1, width * 2 + 2],
+        [width, width + 1, width * 2, width * 2 + 1]
     ];
 
     const iTetromino = [
-        [1, width+1, width*2+1, width*3+1],
-        [width, width+1, width+2, width+3],
-        [2, width+2, width*2+2, width*3+2],
-        [width*2, width*2+1, width*2+2, width*2+3]
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [2, width + 2, width * 2 + 2, width * 3 + 2],
+        [width * 2, width * 2 + 1, width * 2 + 2, width * 2 + 3]
     ];
 
     const theTetrominoes = [lTetromino, zTetromino, tTetromino, bTetromino, iTetromino];
@@ -60,19 +60,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //add the tetromino to the grid with a draw function
     function draw() {
-        current.forEach( index => {
-          blox[currentPosition + index].classList.add('tetromino');
+        current.forEach(index => {
+            blox[currentPosition + index].classList.add('tetromino');
         });
     }
-   // draw(); //testing to see if a random tetromino appears on the grid
+    // draw(); //testing to see if a random tetromino appears on the grid
 
     //remove the tetromino from the grid with an undraw function
     function undraw() {
-        current.forEach( index => {
-          blox[currentPosition + index].classList.remove('tetromino');
+        current.forEach(index => {
+            blox[currentPosition + index].classList.remove('tetromino');
         });
-    }        
+    }
     //add interval to move the tetromino down the grid
+    //this will be used to introduce difficulty by speeding the drop rate up
     timerId = setInterval(moveDown, 1000);
 
     //create a function to move the tetrominoes down the grid
@@ -85,14 +86,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //create a function that freezes the tetromino when it hits the bottom of the div
     function freeze() {
-        if(current.some(index => blox[currentPosition + index + width].classList.contains('taken'))) {
+        if (current.some(index => blox[currentPosition + index + width].classList.contains('taken'))) {
 
-          current.forEach(index => blox[currentPosition + index].classList.add('taken')); 
-          //introduce a new tetromino to the grid
-          random = Math.floor(Math.random() * theTetrominoes.length);
-          current = theTetrominoes[random][currentRotation];
-          currentPosition = 3;
-          draw();
+            current.forEach(index => blox[currentPosition + index].classList.add('taken'));
+            //introduce a new tetromino to the grid
+            random = Math.floor(Math.random() * theTetrominoes.length);
+            current = theTetrominoes[random][currentRotation];
+            currentPosition = 3;
+            draw();
         }
     }
 
@@ -100,19 +101,21 @@ document.addEventListener('DOMContentLoaded', () => {
     function moveLeft() {
         undraw();
         const leftEdge = current.some(index => (currentPosition + index) % width === 0); //check to make sure the tetromino does not exceed the left edge
-        if (!leftEdge) currentPosition -=1;
+        if (!leftEdge) currentPosition -= 1;
         if (current.some(index => blox[currentPosition + index].classList.contains('taken'))) {
-            currentPosition +=1;
+            currentPosition += 1;
         }
         draw();
     }
 
     //create a function to bind directional movement to the arrow keys on the keyboard
     function control(event) {
-        if(event.keyCode === 37) {
+        if (event.keyCode === 37) {
             moveLeft();
-        } else if(event.keyCode === 39) {
+        } else if (event.keyCode === 39) {
             moveRight();
+        } else if (event.keyCode === 40) {
+            moveDown();
         }
     }
     //create an event listener to listen for keypresses and invoke the control fucntions
@@ -121,10 +124,10 @@ document.addEventListener('DOMContentLoaded', () => {
     //create a function to move the teromino to the right until it reaches the edge of the grid
     function moveRight() {
         undraw();
-        const rightEdge = current.some(index => (currentPosition + index) % width === width -1); //check to make sure the tetromino does not exceed the right edge
-        if (!rightEdge) currentPosition +=1;
+        const rightEdge = current.some(index => (currentPosition + index) % width === width - 1); //check to make sure the tetromino does not exceed the right edge
+        if (!rightEdge) currentPosition += 1;
         if (current.some(index => blox[currentPosition + index].classList.contains('taken'))) {
-            currentPosition -=1;
+            currentPosition -= 1;
         }
         draw();
     }
